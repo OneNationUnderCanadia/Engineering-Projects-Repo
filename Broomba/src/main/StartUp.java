1b867cb8c04c8a9de3b9b7dbf50dd576e37c7a20
OneNationUnderCanadia <DanvilleBrianK@gmail.com>
2015-03-24 08:16:44 -0400

    I'll bet you can't tell what I changed ^_^

--- Broomba/src/main/StartUp.java
+++ Broomba/src/main/StartUp.java
@@ -26,7 +26,7 @@ public class StartUp {
 		/** TODO headlights
 		 *  Brian do the thing
 		 *  Joey do the thing
-		 *  Jeff don't do the thing you'd screw it up learn to Java
+		 *  Jeff don't do the thing, you'd screw it up, learn to Java
 		 */
 		
 		// cf = new TetrixControllerFactory(SensorPort.S1);

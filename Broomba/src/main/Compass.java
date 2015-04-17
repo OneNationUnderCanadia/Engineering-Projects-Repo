3658af1ccfc637c4c0c81e77315bc0aeec3978da
jdspillers <jdspillers@bsu.edu>
2015-04-17 08:15:35 -0400

    Now it does stuff

--- Broomba/src/main/Compass.java
+++ Broomba/src/main/Compass.java
@@ -1,5 +1,41 @@
 package main;
 
-public class Compass {
+import lejos.nxt.BasicMotorPort;
+import lejos.nxt.Button;
+import lejos.nxt.LightSensor;
+import lejos.nxt.MotorPort;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.Delay;
 
+public class Compass {
+	LightSensor light;
+	GUI gui;
+	DifferentialPilot marvin;
+	int high;
+	int low;
+	
+	public Compass(LightSensor l1, DifferentialPilot mad){
+		light = l1;
+		gui = new GUI();
+		marvin = mad;
+		
+	}
+	
+	public void goNorth(){
+			MotorPort.A.controlMotor(BasicMotorPort.MAX_POWER, 1); // turn light on
+			while(light.readNormalizedValue() > low){
+				marvin.rotate(2);
+				Delay.msDelay(20);
+			}
+	}
+	
+	public void calibrate(){
+		gui.execute("Put to North & <button> pls");
+		Button.waitForAnyPress();
+		light.calibrateLow();
+		low = light.readNormalizedValue();
+		marvin.rotate(25);
+		light.calibrateHigh();
+		high = light.readNormalizedValue();
+	}
 }
